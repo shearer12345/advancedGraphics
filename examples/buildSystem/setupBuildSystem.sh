301b15995847105173861697196c0b8f95d5e968
John Shearer <shearer12345@gmail.com>
2016-10-07 12:40:21 +0100

    add SDL2_ttf

--- examples/buildSystem/setupBuildSystem.sh
+++ examples/buildSystem/setupBuildSystem.sh
@@ -1,15 +1,23 @@
-echo "== Moving to *build* directory - creating if needed"
+#!/bin/bash
+
+printf "== Moving to *build* directory - creating if needed ... "
 mkdir -p build
+  printf "OK\n"
 cd build
 
 
 ###
-echo "== Installing Packages"
-conan install .. --build missing
+printf "== Installing Packages\n"
+if conan install .. --build missing ; then
+  printf "== Installed Packages OK\n"
+else
+  printf "== FAILED to Install Packages - Exiting\n"
+  exit 1
+fi
 
 
 ###
-echo "== Making build system"
+printf "== Creating build system\n"
 
 # TODO - remove the DCONAN_DISABLE_CHECK_COMPILER flag
 #        - some issue giving error:
@@ -20,14 +28,27 @@ echo "== Making build system"
 #        - possible releated issues:
 #          - https://github.com/conan-io/conan/issues/199
 #          - https://github.com/conan-io/conan/issues/178
-cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCONAN_DISABLE_CHECK_COMPILER=On
+if cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCONAN_DISABLE_CHECK_COMPILER=On ; then
+  printf "== Created build system OK\n"
+else
+  printf "== FAILED to created build system - Exiting\n"
+  exit 1
+fi
+
 
 
 ###
-echo "== Autobuilding (smoke test)"
+printf "== Autobuilding (smoke test)\n"
 
-cmake --build . --config Release
+if cmake --build . --config Release; then
+  printf "== Autobuild was OK\n"
+else
+  printf "== FAILED to Autobuild\n - Exiting\n"
+  exit 1
+fi
 
 ###
-echo "== Return to child directory"
+printf "== Return to child directory ..."
 cd ..
+printf "OK\n"
+exit 0
